b3224f25737e97059f9d15986da54330b7c1c89a
Daniel Mateo Suarez Alvarez <dsuarez@adgasd32.int>
2024-01-22 11:43:37 -0500

    model curso add

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id();//integer columna de id
+            $table->string('name');//columna de nombre tipo varchar y usando la , le podemos asignar cuantos datos debe de almacenar
+            // $table->text('description'); text es para especificar el uso de textos largos
+            $table->string('email')->unique();//propiedad unique para evitar el registro de correos que sean similares
+            $table->timestamp('email_verified_at')->nullable();//metodo timestamp para registrar la fecha en  que se hizo el movimiento y nullable se usa cuando creas datos vacios
+            $table->string('password');//almacenamiento de contraseñas
+            $table->rememberToken();//para el almecenamiento de tokens
+            $table->timestamps();//genera dos columnas una de create_at para registrar fecha de creacion y update_at para registro de actualizaciones, mas ser un control de versiones capaz de revertir cambios
         });
     }
 
---
+++ database/migrations/2024_01_22_153223_create_cursos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    //php artisan make:migration create_cursos_table ya agrega con la clase esquema 
+    //php artisan make:migration add_avatar_to_users_table 
+    public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text("description");
+            $table->integer("curso")->unique();
+            $table->timestamps();
+        });
+        // php artisan migrate:fresh = recorrera todas las tablas y las volvera a migrar, ya con sus cambios
+        // solo se ejecutan en la fase de desarrollo ya que puedo borrar todos los registros
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //metodo que si existe una tabla con el nombre similar la eliminara
+        Schema::dropIfExists('cursos');
+        //php artisan migrate:rollback comando para eliminar las ultimas tablas de la base de datos
+    }
+};
